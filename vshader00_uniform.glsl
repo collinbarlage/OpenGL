4651a56b45197604b4e57fb09f0ae8ff46561f06
Collin Barlage <cjb356@drexel.edu>
2018-01-29 20:23:06 -0500

    Fixed uniform shader

--- vshader00_uniform.glsl
+++ vshader00_uniform.glsl
@@ -1,10 +1,10 @@
 #version 150
 
-in vec2 vPosition;
+in vec4 vPosition;
 
 
 
 void main() 
 { 
-  gl_Position = vec4(vPosition.x, vPosition.y, 0, 1);
+  gl_Position = vPosition;
 }
--- vshader00_v150.glsl
+++ vshader00_v150.glsl
@@ -4,8 +4,8 @@ in vec2 vPosition;
 in vec4 cPosition;
 out vec4 color;
 
-void main() 
-{ 
+void main()
+{
   gl_Position = vec4(vPosition.x, vPosition.y, 0, 1);
   color = cPosition;
 }
